941bb1f01ab9be75e701093cdd45bf1ab41f3067
Smyan Seth <173274810+smyan-seth@users.noreply.github.com>
2025-10-29 11:07:33 -0400

    Add files via upload

---
+++ mood-journal.py
@@ -0,0 +1,6 @@
+from datetime import date
+entry=input('How was your day?\n')
+mood=int(input('How is your mood today on a scale of 1-10?\n1 being the worst, 10 being amazing: '))
+with open('journal.txt','a') as journal:
+    journal.write(f"Date: {date.today()}\nEntry: {entry}\nMood: {mood}\n")
+print('Entry recorded')
